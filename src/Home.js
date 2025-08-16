c0622cd051c2097b392b3bad5a9fc04b680177bf
Ilia Poliak <iliapoliak@proton.me>
2025-08-16 15:30:05 +0200

    fixed layout issue

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="flex items-end justify-center min-h-screen">
+    <div className="fixed inset-0 flex items-end justify-center">
       <Link
         className="text-white bg-green-600 px-5 pt-3 pb-4 m-[5vh] rounded-3xl font-bold  hover:bg-green-700  active:shadow-2xl"
         to="/scan"
