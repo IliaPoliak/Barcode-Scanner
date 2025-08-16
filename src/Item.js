be84af9c1e46cf56dfaa7991355955dc2102c571
Ilia Poliak <iliapoliak@proton.me>
2025-08-16 14:39:00 +0200

    added link from item back to the scanner

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,19 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Item = () => {
   const { isbn } = useParams();
-  return <div>Item: {isbn}</div>;
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <Link
+        className="text-white bg-black px-5 py-3 m-5 rounded-3xl font-bold  hover:bg-[#222]  active:shadow-2xl"
+        to="/"
+      >
+        Scan Another Item
+      </Link>
+
+      <div>Item: {isbn}</div>
+    </div>
+  );
 };
 export default Item;
