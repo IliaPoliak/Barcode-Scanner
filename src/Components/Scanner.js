95bcd9fc9dd957db49b2a009b52513e81ae9d697
Ilia Poliak <iliapoliak@proton.me>
2025-09-02 17:13:50 +0200

    fixed error logging in scanner component

--- src/Components/Scanner.js
+++ src/Components/Scanner.js
@@ -3,28 +3,33 @@ import { Link, useNavigate } from "react-router-dom";
 import { BrowserMultiFormatReader } from "@zxing/library";
 
 const Scanner = () => {
-  const navigate = useNavigate();
-
-  const videoRef = useRef(null);
-  const reader = useRef(new BrowserMultiFormatReader());
+  const videoRef = useRef(null); // reference to <video> element
+  const reader = useRef(new BrowserMultiFormatReader()); // ZXing scanner instance
+  const navigate = useNavigate(); // router navigation
 
   useEffect(() => {
-    if (!videoRef.current) return;
+    if (!videoRef.current) return; // if the <video> element isn’t mounted yet
+
+    // Start the camera
     reader.current.decodeFromConstraints(
       {
         audio: false,
         video: {
-          facingMode: "environment",
+          facingMode: "environment", // use back camera
         },
       },
       videoRef.current,
       (result, error) => {
-        if (result) navigate(`/item/${result.text}`);
-        if (error) console.log(error);
+        if (result) {
+          navigate(`/item/${result.text}`); // if found navigate to item page
+        } else if (error && !(error.name === "NotFoundException")) {
+          console.log(error);
+        }
       }
     );
+
     return () => {
-      reader.current.reset();
+      reader.current.reset(); // stop scanner on unmount
     };
   }, [videoRef]);
 
